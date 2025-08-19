7c9c8cd5a56ba56ad30364acc3ffff529ec2b7f3
Luiz <luiz.vasconcelos@openzeppelin.com>
2025-08-19 16:25:29 -0300

    Broadcaster

--- contracts/Broadcaster.sol
+++ contracts/Broadcaster.sol
@@ -3,44 +3,45 @@ pragma solidity ^0.8.27;
 
 import {IBroadcaster} from "./interfaces/IBroadcaster.sol";
 
-
+import {StorageSlot} from "openzeppelin/utils/StorageSlot.sol";
 
 contract Broadcaster is IBroadcaster {
+    error MessageAlreadyBroadcasted();
+
     function broadcastMessage(bytes32 message) external {
         // calculate the storage slot for the message
-        uint256 slot = _messageSlot(message, msg.sender);
+        bytes32 slot = _computeMessageSlot(message, msg.sender);
 
         // ensure the message has not already been broadcast
-        require(_sload(slot) == 0, "Broadcaster: message already broadcasted");
+
+        if (_loadStorageSlot(slot) != 0) {
+            revert MessageAlreadyBroadcasted();
+        }
 
         // store the message and its timestamp
-        _sstore(slot, block.timestamp);
+        _writeStorageSlot(slot, block.timestamp);
 
         // emit the event
         emit MessageBroadcast(message, msg.sender);
     }
 
-    /// @notice Not required by the standard, but useful for visibility. TODO: consider adding to the standard.
+    /// @dev Not required by the standard, but useful for visibility.
     function hasBroadcasted(bytes32 message, address publisher) external view returns (bool) {
-        return _sload(_messageSlot(message, publisher)) != 0;
+        return _loadStorageSlot(_computeMessageSlot(message, publisher)) != 0;
     }
 
     /// @dev Helper function to store a value in a storage slot.
-    function _sstore(uint256 s, uint256 v) internal {
-        assembly {
-            sstore(s, v)
-        }
+    function _writeStorageSlot(bytes32 slot, uint256 value) internal {
+        StorageSlot.getUint256Slot(slot).value = value;
     }
 
     /// @dev Helper function to load a storage slot.
-    function _sload(uint256 s) internal view returns (uint256 r) {
-        assembly {
-            r := sload(s)
-        }
+    function _loadStorageSlot(bytes32 slot) internal view returns (uint256 value) {
+        value = StorageSlot.getUint256Slot(slot).value;
     }
 
     /// @dev Helper function to calculate the storage slot for a message.
-    function _messageSlot(bytes32 message, address publisher) internal pure returns (uint256) {
-        return uint256(keccak256(abi.encode(message, publisher)));
+    function _computeMessageSlot(bytes32 message, address publisher) internal pure returns (bytes32) {
+        return keccak256(abi.encode(message, publisher));
     }
 }
