52ff90b39fdd2e142a8d61a4e83577eb113bc3f4
Luiz <luiz.vasconcelos@openzeppelin.com>
2025-08-19 18:05:08 -0300

    add BlockHashProverPointer

---
+++ contracts/BlockHashProverPointer.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {StorageSlot} from "openzeppelin/utils/StorageSlot.sol";
+import {IBlockHashProverPointer} from "./interfaces/IBlockHashProverPointer.sol";
+
+abstract contract BlockHashProverPointer is IBlockHashProverPointer {
+
+    bytes32 constant BLOCK_HASH_PROVER_POINTER_SLOT = bytes32(uint256(keccak256("eip7888.pointer.slot")) - 1);
+
+    address private _implementationAddress;
+
+    function implementationAddress() external view returns (address) {
+        return _implementationAddress;
+    }
+
+    /// @notice Return the code hash of the latest version of the prover.
+    function implementationCodeHash() external view returns (bytes32 codeHash) {
+        codeHash = StorageSlot.getBytes32Slot(BLOCK_HASH_PROVER_POINTER_SLOT).value;
+    }
+}
