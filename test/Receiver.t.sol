c055fbf381398fc07dda5606f8e87d5746650467
Luiz <luiz.vasconcelos@openzeppelin.com>
2025-11-06 16:11:41 -0300

    update proofs

--- test/Receiver.t.sol
+++ test/Receiver.t.sol
@@ -290,7 +290,7 @@ contract ReceiverTest is Test {
 
         uint256 expectedSlot = uint256(keccak256("eip7888.pointer.slot")) - 1;
 
-        string memory path = "test/payloads/ethereum/arb_pointer_proof_block_9561476.json";
+        string memory path = "test/payloads/ethereum/arb_pointer_proof_block_9574620.json";
 
         string memory json = vm.readFile(path);
         uint256 blockNumber = json.readUint(".blockNumber");
@@ -368,7 +368,7 @@ contract ReceiverTest is Test {
         {
             uint256 expectedSlot = uint256(keccak256("eip7888.pointer.slot")) - 1;
 
-            string memory path = "test/payloads/ethereum/arb_pointer_proof_block_9561476.json";
+            string memory path = "test/payloads/ethereum/arb_pointer_proof_block_9574620.json";
 
             string memory json = vm.readFile(path);
             uint256 blockNumber = json.readUint(".blockNumber");
