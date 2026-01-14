0e8afd5cab407765f26f17e66b69cd1acc1594b8
Luiz <luiz.vasconcelos@openzeppelin.com>
2026-01-14 16:05:46 -0300

    docs

--- src/contracts/block-hash-pusher/zksync/ZkSyncBuffer.sol
+++ src/contracts/block-hash-pusher/zksync/ZkSyncBuffer.sol
@@ -4,16 +4,33 @@ pragma solidity 0.8.28;
 import {BaseBuffer} from "../BaseBuffer.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {AddressAliasHelper} from "@arbitrum/nitro-contracts/src/libraries/AddressAliasHelper.sol";
+import {IBuffer} from "../interfaces/IBuffer.sol";
 
+/// @title ZkSyncBuffer
+/// @notice Implementation of BaseBuffer for storing Ethereum L1 block hashes on ZkSync Era L2.
+/// @dev This contract extends BaseBuffer with access control specific to ZkSync's L1->L2 messaging.
+///      When a message is sent from L1 to L2 via ZkSync's Mailbox, the sender address is aliased.
+///      The buffer only accepts hash pushes from the aliased pusher address.
+/// @notice The contract is `Ownable` but the ownership is renounced after the pusher address is set.
+///         This ensures that the pusher address is set only once and cannot be changed.
 contract ZkSyncBuffer is BaseBuffer, Ownable {
+    /// @dev The address of the pusher contract on L1.
     address private _pusherAddress;
 
+    /// @notice Thrown when attempting to receive hashes before the pusher address has been set.
     error PusherAddressNotSet();
 
+    /// @notice Emitted when the pusher address is set and ownership is renounced.
+    /// @param pusherAddress The address of the pusher contract on L1.
     event PusherAddressSet(address pusherAddress);
 
     constructor(address initialOwner_) Ownable(initialOwner_) {}
 
+    /// @notice Sets the pusher address and renounces ownership.
+    /// @dev This function can only be called once by the owner. After setting the pusher address,
+    ///      ownership is renounced to prevent further modifications. The pusher address is used to
+    ///      derive the aliased pusher address that will be authorized to push hashes.
+    /// @param pusherAddress_ The address of the ZkSyncPusher contract on L1.
     function setPusherAddress(address pusherAddress_) external onlyOwner {
         _pusherAddress = pusherAddress_;
 
@@ -21,6 +38,7 @@ contract ZkSyncBuffer is BaseBuffer, Ownable {
         renounceOwnership();
     }
 
+    /// @inheritdoc IBuffer
     function receiveHashes(uint256 firstBlockNumber, bytes32[] calldata blockHashes) external {
         if (_pusherAddress == address(0)) {
             revert PusherAddressNotSet();
@@ -33,10 +51,12 @@ contract ZkSyncBuffer is BaseBuffer, Ownable {
         _receiveHashes(firstBlockNumber, blockHashes);
     }
 
+    /// @inheritdoc IBuffer
     function pusher() public view returns (address) {
         return _pusherAddress;
     }
 
+    /// @notice The aliased address of the pusher contract on L2.
     function aliasedPusher() public view returns (address) {
         return AddressAliasHelper.applyL1ToL2Alias(_pusherAddress);
     }
