0d8202531998d0174cbce2a62970d2945954a026
Luiz <luiz.vasconcelos@openzeppelin.com>
2026-01-22 15:28:59 -0300

    up

--- src/contracts/block-hash-pusher/interfaces/IBuffer.sol
+++ src/contracts/block-hash-pusher/interfaces/IBuffer.sol
@@ -7,13 +7,13 @@ pragma solidity ^0.8.28;
 ///         until another block X+M*bufferSize is pushed, where M is a positive integer.
 /// @notice Inspired by: https://github.com/OffchainLabs/block-hash-pusher/blob/main/contracts/interfaces/IBuffer.sol
 interface IBuffer {
-    /// @notice Thrown when the block hashes array is empty.
-    error EmptyBlockHashes();
-
     /// @notice Emitted when the buffer is pushed to.
     /// @param  firstBlockNumber The block number of the first block in the batch.
     /// @param  lastBlockNumber The block number of the last block in the batch.
     event BlockHashesPushed(uint256 firstBlockNumber, uint256 lastBlockNumber);
+
+    /// @notice Thrown when the block hashes array is empty.
+    error EmptyBlockHashes();
 
     /// @notice Thrown by `parentChainBlockHash` when the block hash for a given block number is not found.
     error UnknownParentChainBlockHash(uint256 parentChainBlockNumber);
