47ba2eaf3b5807e1eef8cfda2008d41641a439ec
Luiz <luiz.vasconcelos@openzeppelin.com>
2026-02-23 18:22:58 -0300

    up

--- src/contracts/provers/scroll/ParentToChildProver.sol
+++ src/contracts/provers/scroll/ParentToChildProver.sol
@@ -86,9 +86,7 @@ contract ParentToChildProver is IStateProver {
         // Get the state root from ScrollChain
         targetStateCommitment = IScrollChain(scrollChain).finalizedStateRoots(batchIndex);
 
-        if (targetStateCommitment == bytes32(0)) {
-            revert StateRootNotFound();
-        }
+        require(targetStateCommitment != bytes32(0), InvalidTargetStateCommitment());
     }
 
     /// @notice Verify a storage slot given an L2 state root and a proof
